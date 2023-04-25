99aae14cff9ee361fb1f7e1273ba8c883a12b2d7
DUONG TIEN HIEU <tienhieuD@users.noreply.github.com>
2023-04-25 11:38:40 +0700

    .

--- scripts/mongo_setup.sh
+++ scripts/mongo_setup.sh
@@ -9,17 +9,17 @@ mongo <<EOF
     "members": [
       {
         "_id": 0,
-        "host": "mongo1",
+        "host": "mongo1:27017",
         "priority": 2
       },
       {
         "_id": 1,
-        "host": "mongo2",
+        "host": "mongo2:27017",
         "priority": 0
       },
       {
         "_id": 2,
-        "host": "mongo3",
+        "host": "mongo3:27017",
         "priority": 0
       }
     ]
